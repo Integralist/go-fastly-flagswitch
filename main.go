055e6506cdb5297faf110c04fe20512dcb7e767a
Integralist <mark.mcdx@gmail.com>
2018-09-14 18:22:13 +0100

    Create dictionaries as well

--- main.go
+++ main.go
@@ -9,13 +9,148 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 )
+
+type service struct {
+	Name       string `json:"name"`
+	ID         string `json:"id"`
+	Dictionary string `json:"dictionary,omitempty"`
+}
+
+type config struct {
+	Services []service `json:"services"`
+}
+
+type fastlyServiceVersion struct {
+	Active    bool   `json:"active"`
+	Comment   string `json:"comment"`
+	Created   string `json:"created_at"`
+	Deleted   string `json:"deleted_at"`
+	Deployed  bool   `json:"deployed"`
+	Locked    bool   `json:"locked"`
+	Number    int    `json:"number"`
+	ServiceID string `json:"service_id"`
+	Staging   bool   `json:"staging"`
+	Testing   bool   `json:"testing"`
+	Updated   string `json:"updated_at"`
+}
+
+type fastlyResponse struct {
+	Versions []fastlyServiceVersion `json:"versions"`
+}
+
+type fastlyDictionary struct {
+	Created   string `json:"created_at"`
+	Deleted   string `json:"deleted_at"`
+	ID        string `json:"id"`
+	Name      string `json:"name"`
+	ServiceID string `json:"service_id"`
+	Updated   string `json:"updated_at"`
+	Version   int    `json:"version"`
+	Write     bool   `json:"write_only"`
+}
 
 var info = log.New(os.Stdout, "", log.Ldate|log.Ltime|log.Lshortfile)
 
 var netClient = &http.Client{
 	Timeout: time.Second * 10,
+}
+
+var apiHost = "https://api.fastly.com"
+
+func normaliseName(serviceName string) string {
+	replacer := strings.NewReplacer(".", "_", "-", "_")
+	return replacer.Replace(serviceName)
+}
+
+func apiCall(apiToken string, url, method string, data *bytes.Buffer) string {
+	req, err := http.NewRequest(method, url, data)
+	if err != nil {
+		log.Fatal(fmt.Sprintf("error creating %s request: %v", method, err))
+	}
+
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("Fastly-Key", apiToken)
+
+	resp, err := netClient.Do(req)
+	if err != nil {
+		log.Fatal(fmt.Sprintf("error making %s request: %v", method, err))
+	}
+
+	defer resp.Body.Close()
+
+	contents, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(fmt.Sprintf("error reading %s response: %v", method, err))
+	}
+
+	return string(contents)
+}
+
+func getLatestServiceVersion(srv service, apiToken string) fastlyServiceVersion {
+	url := fmt.Sprintf("%s/service/%s", apiHost, srv.ID)
+	nodata := bytes.NewBufferString("")
+	result := apiCall(apiToken, url, "GET", nodata)
+
+	fr := &fastlyResponse{}
+	json.Unmarshal([]byte(result), fr)
+
+	lastItem := fr.Versions[len(fr.Versions)-1]
+
+	return lastItem
+}
+
+func cloneLatestVersion(srv service, apiToken string) int {
+	latestVersion := getLatestServiceVersion(srv, apiToken)
+
+	if latestVersion.Active == false {
+		info.Println("don't bother cloning as we just need a non-active service version in order to create an edge dictionary")
+		return latestVersion.Number
+	}
+
+	url := fmt.Sprintf("%s/service/%s/version/%d/clone", apiHost, srv.ID, latestVersion.Number)
+	nodata := bytes.NewBufferString("")
+	result := apiCall(apiToken, url, "PUT", nodata)
+
+	fsv := &fastlyServiceVersion{}
+	json.Unmarshal([]byte(result), fsv)
+
+	return fsv.Number
+}
+
+func getDictionaryForService(nonActiveVersion int, srv service, apiToken string) string {
+	nodata := bytes.NewBufferString("")
+	url := fmt.Sprintf("%s/service/%s/version/%d/dictionary/%s", apiHost, srv.ID, nonActiveVersion, normaliseName(srv.Name))
+
+	result := apiCall(apiToken, url, "GET", nodata)
+
+	fd := &fastlyDictionary{}
+	json.Unmarshal([]byte(result), fd)
+
+	info.Printf("successfully retrieved pre-existing edge dictionary for %s\n", srv.Name)
+
+	return fd.ID
+}
+
+func createDictionaryForService(srv service, apiToken string) string {
+	nonActiveVersion := cloneLatestVersion(srv, apiToken)
+	dictionaryID := getDictionaryForService(nonActiveVersion, srv, apiToken)
+
+	if dictionaryID != "" {
+		return dictionaryID
+	}
+
+	data := bytes.NewBufferString(fmt.Sprintf("name=%s", normaliseName(srv.Name)))
+	url := fmt.Sprintf("%s/service/%s/version/%d/dictionary", apiHost, srv.ID, nonActiveVersion)
+
+	result := apiCall(apiToken, url, "POST", data)
+
+	fd := &fastlyDictionary{}
+	json.Unmarshal([]byte(result), fd)
+
+	return fd.ID
 }
 
 func switchKeyValue(val bool, srv service, apiToken string) {
@@ -26,7 +161,6 @@ func switchKeyValue(val bool, srv service, apiToken string) {
 
 	info.Printf("attempting to serve %s (%s) from %s", srv.Name, srv.ID, location)
 
-	apiHost := "https://api.fastly.com"
 	dictKey := "west"
 	data := bytes.NewBufferString(fmt.Sprintf("item_value=%t", val))
 	url := fmt.Sprintf("%s/service/%s/dictionary/%s/item/%s", apiHost, srv.ID, srv.Dictionary, dictKey)
@@ -51,17 +185,7 @@ func switchKeyValue(val bool, srv service, apiToken string) {
 		log.Fatal(fmt.Sprintf("error reading PATCH response: %v", err))
 	}
 
-	fmt.Printf("\n%+v\n", string(contents))
-}
-
-type service struct {
-	Name       string `json:"name"`
-	ID         string `json:"id"`
-	Dictionary string `json:"dictionary"`
-}
-
-type config struct {
-	Services []service `json:"services"`
+	fmt.Printf("%+v\n\n", string(contents))
 }
 
 func loadConfig(path string) *config {
@@ -101,6 +225,17 @@ func main() {
 	}
 
 	for _, srv := range data.Services {
+		if srv.Dictionary == "" {
+			dictionaryID := createDictionaryForService(srv, apiToken)
+
+			if dictionaryID == "" {
+				msg := "hmm, we failed to create a dictionary for the non-active service"
+				log.Fatal(fmt.Sprintf("%s: %s", msg, srv.Name))
+			}
+
+			srv.Dictionary = dictionaryID
+		}
+
 		switchKeyValue(val, srv, apiToken)
 	}
 }
